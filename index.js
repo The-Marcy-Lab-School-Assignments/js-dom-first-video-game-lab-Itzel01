740b44595b6f44c158bfd831b3e03530c0a4dd37
Itzel01 <itzel.areymi.rendon@gmail.com>
2021-01-28 20:18:50 -0500

    finished the most basic form of this game

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-let square = document.getElementById("square");
-console.log(square.style.left, square.style.top);
+// let square = document.getElementById("square");
+// console.log(square.style.left, square.style.top);
 
-document.addEventListener("keydown", function(e) {
-  console.log(e.key)
+// document.addEventListener("keydown", (e) => {
+//     console.log(e.key);
+// });
+
+document.addEventListener("keydown", (e) => { 
+
+    if(e.key === "ArrowRight" && square.style.left !== "300px" ){
+      square.style.left = parseInt(square.style.left)+10 + "px";
+    } else{
+      
+    }
+    if(e.key === "ArrowLeft" && square.style.left !== "0px"){
+      square.style.left = parseInt(square.style.left)-10 + "px";
+    }
+    if(e.key === "ArrowUp" && square.style.top !== "0px"){
+      square.style.top=parseInt(square.style.top)-10 +'px';
+    }
+    if(e.key === "ArrowDown" && square.style.top !== "300px"){
+      square.style.top=parseInt(square.style.top)+10 +'px';
+    }
 });
+
+square.addEventListener("click", () => {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  square.style.backgroundColor = "#" + randomColor;
+})
